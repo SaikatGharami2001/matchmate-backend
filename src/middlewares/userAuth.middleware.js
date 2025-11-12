6964db3e32333cdba11314c2a1fa0a60f372cece
SaikatGharami2001 <saikatgharami2@gmail.com>
2025-11-12 05:40:03 +0530

    Update: Fixed userAuth middleware

--- src/middlewares/userAuth.middleware.js
+++ src/middlewares/userAuth.middleware.js
@@ -2,13 +2,21 @@ const jwt = require("jsonwebtoken");
 const UserModel = require("../models/User.model");
 
 const userAuth = async (req, res, next) => {
-  const { token } = req.cookies;
-  const verifyToken = jwt.verify(token, "secret");
-  const { _id } = verifyToken;
-  const loggedInUser = await UserModel.findById(_id);
-  if (!loggedInUser) return res.status(401).json({ Error: "User not found" });
-  req.user = loggedInUser;
-  next();
+  try {
+    const { token } = req.cookies;
+    if (!token) return res.status(401).json({ Error: "token not found" });
+
+    const verifyToken = jwt.verify(token, "secret");
+    const { _id } = verifyToken;
+    const loggedInUser = await UserModel.findById(_id);
+
+    if (!loggedInUser) return res.status(401).json({ Error: "User not found" });
+
+    req.user = loggedInUser;
+    next();
+  } catch (err) {
+    res.status(401).json({ Error: err.message });
+  }
 };
 
 module.exports = userAuth;
