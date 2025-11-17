b64d7eca5ba57357137354715a6f80e204949fb5
SaikatGharami2001 <saikatgharami2@gmail.com>
2025-11-17 08:53:52 +0530

    Update: added the cors middleware for frontend

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -43,4 +43,12 @@ userRoutes.get("/user/connections", userAuth, async (req, res) => {
   }
 });
 
+userRoutes.get("/feed", userAuth, async (req, res) => {
+  try {
+    res.status(200).json({ Message: "Connections" });
+  } catch (err) {
+    res.status(500).json({ Error: err.message });
+  }
+});
+
 module.exports = userRoutes;
