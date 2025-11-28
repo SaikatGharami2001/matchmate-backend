10ca00de787e75a1af6c15203e7e46f4d74cc53f
SaikatGharami2001 <saikatgharami2@gmail.com>
2025-11-28 09:37:41 +0530

    Update: Secket.io connectione setup created for backend

--- src/server.js
+++ src/server.js
@@ -1,6 +1,22 @@
 require("dotenv").config();
+
 const app = require("./app");
+const http = require("http");
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT;
+const server = http.createServer(app);
 
-app.listen(PORT, () => console.log(`The server is running in ${PORT}`));
+const socket = require("socket.io");
+const io = socket(server, { cors: { origin: "http://localhost:5174" } });
+
+io.on("connection", (socket) => {
+  socket.on("joinChat", () => {});
+  socket.on("sendMessage", () => {});
+  socket.on("disconnect", () => {});
+});
+
+server.listen(PORT, () => console.log(`The server is running in ${PORT}`));
+
+server.on("error", (err) =>
+  console.error("Server failed to start:", err.message)
+);
